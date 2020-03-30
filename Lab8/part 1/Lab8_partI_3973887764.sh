13a8543ec4755c463144fb097804d1e82bf16763
Inayyyy <jinyani1996@gmail.com>
2020-03-30 12:10:32 -0700

    update the answer to problem 4

--- Lab8/part 1/Lab8_partI_3973887764.sh
+++ Lab8/part 1/Lab8_partI_3973887764.sh
@@ -16,9 +16,7 @@ egrep '^[0-9]{2}(.|-|/)[A-Z][a-z]{2}\1[0-9]{4}$' input3.txt > output3.txt
 #################################################################################
 # https://www.geeksforgeeks.org/sed-command-in-linux-unix-with-examples/
 # Problem 4: find suitable course
-touch output4.txt
-cp ee_classes.html test.html
-cat test.html | sed 's/<div class="course-info expandable"/\n/g' | egrep '.*DEN@Viterbi.*' | egrep '.*<span class="units">\((4|3).0 units\).*' | egrep '.*\"EE-5[0-9]{2}\".*' | egrep -o 'EE [0-9]{3}:</strong>( \b(\w|-)+\b)+ <span class="units">\((4|3).0 units\)' | sed 's/<\/strong>//g' | sed 's/<span.*>//g' | sort -t '(' -k 2 > output4.txt
+curl https://classes.usc.edu/term-20191/classes/ee/ | sed 's/<div class="course-info expandable"/\n/g' | egrep '.*DEN@Viterbi.*' | egrep '.*<span class="units">\((4|3).0 units\).*' | egrep '.*\"EE-5[0-9]{2}\".*' | egrep -o 'EE [0-9]{3}:</strong>( \b(\w|-)+\b)+ <span class="units">\((4|3).0 units\)' | sed 's/<\/strong>//g' | sed 's/<span.*>//g' | sort -t '(' -k 2 > output4.txt
 
 ################################################################################
 # Problem 5: identify comments
